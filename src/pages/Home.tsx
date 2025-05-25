423f57e94af32485c69fe6aab3615e8d981215d5
amberchengphotography <amberchengphotography@gmail.com>
2025-05-25 01:59:29 -0700

    Delete src directory

--- src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-   # Create new React project
-   npx create-react-app portfolio-website --template typescript
-   cd portfolio-website
-   
-   # Install dependencies
-   npm install react-router-dom @emotion/styled framer-motion

8c28b66b25e3703c40af9a4ecb857fb1f41e10b4
amberchengphotography <amberchengphotography@gmail.com>
2025-05-25 01:42:01 -0700

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+   # Create new React project
+   npx create-react-app portfolio-website --template typescript
+   cd portfolio-website
+   
+   # Install dependencies
+   npm install react-router-dom @emotion/styled framer-motion
